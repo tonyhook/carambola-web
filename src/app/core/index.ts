84009357dadfb69e8fac744398b28684e568099c
Tony Hook <tonyhook.su@gmail.com>
2025-03-31 16:20:31 +0800

    [security] security services

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,5 +1,9 @@
 export * from './api/open/security.api';
 export * from './api/managed/menu.api';
+export * from './api/managed/authority.api';
+export * from './api/managed/permission.api';
+export * from './api/managed/role.api';
+export * from './api/managed/user.api';
 
 export * from './datasource/paginated-data-source';
 export * from './datasource/query';
@@ -13,3 +17,7 @@ export * from './entity/backend/menu';
 export * from './entity/backend/menu-tree-node';
 export * from './entity/security/login';
 export * from './entity/security/user-details';
+export * from './entity/security/authority';
+export * from './entity/security/permission';
+export * from './entity/security/role';
+export * from './entity/security/user';
