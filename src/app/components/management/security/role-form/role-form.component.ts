bdf30a556442c825b4e06f2df6916eb9ece06743
Tony Hook <tonyhook.su@gmail.com>
2025-04-11 10:17:32 +0800

    [management] security management

---
+++ src/app/components/management/security/role-form/role-form.component.ts
@@ -0,0 +1,222 @@
+import { Component, effect, input, OnInit, output, signal, WritableSignal } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { Authority, AuthorityAPI, Permission, PermissionAPI, Role, RoleAPI } from '../../../../core';
+import { OperationComponent } from '../../../../shared/components/operation/operation.component';
+
+@Component({
+  selector: 'carambola-role-form',
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatTabsModule,
+    OperationComponent
+  ],
+  templateUrl: './role-form.component.html',
+  styleUrls: ['./role-form.component.scss'],
+})
+export class RoleFormComponent implements OnInit {
+  formGroup: UntypedFormGroup;
+
+  authorities: WritableSignal<Authority[]> = signal([]);
+  resourcesTypes: WritableSignal<string[]> = signal([]);
+
+  roleAuthoritySet: WritableSignal<Set<Authority>> = signal(new Set<Authority>());
+  rolePermissionMap: WritableSignal<Map<string, Permission>> = signal(new Map<string, Permission>());
+
+  displayedAuthorityColumns: string[] = ['name'];
+  displayedResourceTypeColumns: string[] = ['type', 'permission'];
+
+  role = input<Role | null>(null);
+  changed = output<boolean>();
+
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private snackBar: MatSnackBar,
+    private authorityAPI: AuthorityAPI,
+    private permissionAPI: PermissionAPI,
+    private roleAPI: RoleAPI,
+  ) {
+    this.formGroup = this.formBuilder.group({
+      'name': ['', Validators.required],
+    });
+
+    effect(() => {
+      const role = this.role();
+      this.formGroup = this.formBuilder.group({
+        'name': [role ? role.name : '', Validators.required],
+      });
+    });
+
+    effect(() => {
+      const role = this.role();
+      const authorities = this.authorities();
+      const resourcesTypes = this.resourcesTypes();
+
+      const roleAuthoritySet = new Set<Authority>();
+      const rolePermissionMap = new Map<string, Permission>();
+      if (role) {
+        if (role.authorities) {
+          role.authorities.forEach(authority => {
+            roleAuthoritySet.add(authorities.find(a => a.id === authority.id)!);
+          });
+        }
+        this.roleAuthoritySet.set(roleAuthoritySet);
+
+        resourcesTypes.forEach(resourceType => {
+          this.permissionAPI.getClassPermissionList(resourceType).subscribe(permissions => {
+            for (const permission of permissions) {
+              if (permission.roleId === role.id) {
+                rolePermissionMap.set(resourceType, permission);
+              }
+            }
+          });
+        });
+        this.rolePermissionMap.set(rolePermissionMap);
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.authorityAPI.getAuthorityList().subscribe(data => {
+      this.authorities.set(data);
+    });
+    this.permissionAPI.getResourceTypeList().subscribe(data => {
+      this.resourcesTypes.set(data);
+    });
+  }
+
+  addRole() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    const role: Role = {
+      id: null,
+      name: this.formGroup.value.name,
+      createTime: null,
+      updateTime: null,
+      authorities: [],
+    };
+
+    this.roleAPI.addRole(role).subscribe(() => {
+      this.snackBar.open('Role added', 'OK', {
+        duration: 2000,
+      });
+
+      this.changed.emit(true);
+    });
+  }
+
+  updateRole() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    const role = this.role();
+    if (role) {
+      role.name = this.formGroup.value.name;
+
+      this.roleAPI.updateRole(role.id!, role).subscribe(() => {
+        this.snackBar.open('Role updated', 'OK', {
+          duration: 2000,
+        });
+
+        this.changed.emit(true);
+      });
+    }
+  }
+
+  removeRole() {
+    const role = this.role();
+    if (role) {
+      this.roleAPI.removeRole(role.id!).subscribe(() => {
+        this.snackBar.open('Role removed', 'OK', {
+          duration: 2000,
+        });
+
+        this.changed.emit(true);
+      });
+  }
+  }
+
+  cancelRole() {
+    this.changed.emit(false);
+  }
+
+  toggleAuthority(authority: Authority, event: MatCheckboxChange) {
+    const role = this.role();
+
+    if (role) {
+      if (event.checked) {
+        role.authorities.push(authority);
+      } else {
+        const index = role.authorities.findIndex(a => a.id === authority.id);
+        role.authorities.splice(index, 1);
+      }
+
+      this.roleAPI.updateRole(role.id!, role).subscribe();
+    }
+  }
+
+  getPermission(resourceType: string) {
+    const rolePermissionMap = this.rolePermissionMap();
+    const permission = rolePermissionMap.get(resourceType);
+    if (permission) {
+      return permission.permission;
+    } else {
+      return '';
+    }
+  }
+
+  togglePermission(resourceType: string, newOp: string | null) {
+    const role = this.role();
+    if (role) {
+      const rolePermissionMap = this.rolePermissionMap();
+      let permission = rolePermissionMap.get(resourceType);
+
+      if (!permission) {
+        permission = {
+          id: null,
+          resourceType: resourceType,
+          resourceId: null,
+          roleId: role.id,
+          permission: newOp,
+        };
+
+        this.permissionAPI.addPermission(permission).subscribe(data => {
+          rolePermissionMap.set(resourceType, data);
+          this.rolePermissionMap.set(rolePermissionMap);
+        });
+      } else {
+        if (!newOp || newOp.length === 0) {
+          this.permissionAPI.removePermission(permission.id!).subscribe(() => {
+            rolePermissionMap.delete(resourceType);
+            this.rolePermissionMap.set(rolePermissionMap);
+          });
+        } else {
+          permission.permission = newOp;
+          this.permissionAPI.updatePermission(permission.id!, permission).subscribe(() => {
+            this.rolePermissionMap.set(rolePermissionMap);
+          });
+        }
+      }
+    }
+  }
+
+}
---
+++ src/app/components/management/security/user-form/user-form.component.ts
@@ -0,0 +1,175 @@
+import { Component, effect, input, OnInit, output, signal, WritableSignal } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { Role, RoleAPI, User, UserAPI } from '../../../../core';
+
+@Component({
+  selector: 'carambola-user-form',
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatTabsModule,
+  ],
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss'],
+})
+export class UserFormComponent implements OnInit {
+  formGroup: UntypedFormGroup;
+
+  roles: WritableSignal<Role[]> = signal([]);
+
+  userRoleSet: WritableSignal<Set<Role>> = signal(new Set<Role>());
+
+  displayedRoleColumns: string[] = ['name'];
+
+  user = input<User | null>(null);
+  changed = output<boolean>();
+
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private snackBar: MatSnackBar,
+    private roleAPI: RoleAPI,
+    private userAPI: UserAPI,
+  ) {
+    this.formGroup = this.formBuilder.group({
+      'username': ['', Validators.required],
+      'password': ['', Validators.required],
+      'enabled': [true, Validators.required],
+    });
+
+    effect(() => {
+      const user = this.user();
+      this.formGroup = this.formBuilder.group({
+        'username': [user ? user.username : '', Validators.required],
+        'password': [user ? '********' : '', Validators.required],
+        'enabled': [user ? user.enabled : true, Validators.required],
+      });
+    });
+
+    effect(() => {
+      const user = this.user();
+      const roles = this.roles();
+
+      const userRoleSet = new Set<Role>();
+      if (user) {
+        if (user.roles) {
+          user.roles.forEach(role => {
+            userRoleSet.add(roles.find(r => r.id === role.id)!);
+          });
+        }
+        this.userRoleSet.set(userRoleSet);
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.roleAPI.getRoleList().subscribe(data => {
+      this.roles.set(data);
+    });
+  }
+
+  addUser() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    const user: User = {
+      id: null,
+      username: this.formGroup.value.username,
+      password: this.formGroup.value.password,
+      enabled: this.formGroup.value.enabled,
+      createTime: null,
+      updateTime: null,
+      roles: [],
+    };
+
+    this.userAPI.addUser(user).subscribe(() => {
+      this.snackBar.open('User added', 'OK', {
+        duration: 2000,
+      });
+
+      this.changed.emit(true);
+    });
+  }
+
+  updateUser() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    const user = this.user();
+    if (user) {
+      user.username = this.formGroup.value.username;
+      if (this.formGroup.controls['password'].touched) {
+        user.password = this.formGroup.value.password;
+      } else {
+        user.password = null;
+      }
+      user.enabled = this.formGroup.value.enabled;
+
+      this.userAPI.updateUser(user.id!, user).subscribe(() => {
+        this.snackBar.open('User updated', 'OK', {
+          duration: 2000,
+        });
+
+        this.changed.emit(true);
+      });
+    }
+  }
+
+  removeUser() {
+    const user = this.user();
+    if (user) {
+      this.userAPI.removeUser(user.id!).subscribe(() => {
+        this.snackBar.open('User removed', 'OK', {
+          duration: 2000,
+        });
+
+        this.changed.emit(true);
+      });
+    }
+  }
+
+  cancelUser() {
+    this.changed.emit(false);
+  }
+
+  touchPassword() {
+    if (this.formGroup.controls['password'].touched) {
+      return;
+    }
+
+    this.formGroup.controls['password'].setValue('');
+  }
+
+  toggleRole(role: Role, event: MatCheckboxChange) {
+    const user = this.user();
+
+    if (user) {
+      if (event.checked) {
+        user.roles.push(role);
+      } else {
+        const index = user.roles.findIndex(r => r.id === role.id);
+        user.roles.splice(index, 1);
+      }
+
+      this.userAPI.updateUser(user.id!, user).subscribe();
+    }
+  }
+
+}
