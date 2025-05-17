03b65203c5e58ad1836b59013968ac2c70c27d66
Tony Hook <tonyhook.su@gmail.com>
2025-05-17 16:19:27 +0800

    [audit] audit management

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -6,3 +6,5 @@ export * from './components/treeview/treeview.component';
 export * from './pipes/get-role-name.pipe';
 export * from './pipes/get-user-name.pipe';
 export * from './pipes/is-new.pipe';
+
+export * from './localization/date-adapter';
