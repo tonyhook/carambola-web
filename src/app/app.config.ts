6df9afe233a411678e07fbcb49d0d8bbf68cc4b1
Tony Hook <tonyhook.su@gmail.com>
2025-08-15 10:32:46 +0800

    [framework] session timeout interceptor
    refresh page for bouncing to login when receive 401 for session timeout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideCarambolaDateAdapter } from './shared';
+import { HttpErrorInterceptor } from './shared';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptorsFromDi(),
+    ),
     provideAnimationsAsync(),
     provideCarambolaDateAdapter(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    },
   ]
 };
