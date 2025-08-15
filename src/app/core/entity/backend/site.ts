b8fc0d898c279c10539b764980feb732b5a211f1
Tony Hook <tonyhook.su@gmail.com>
2025-08-15 10:33:25 +0800

    [base] site info

---
+++ src/app/core/entity/backend/site.ts
@@ -0,0 +1,7 @@
+export interface Site {
+  id: number;
+  domain: string;
+  name: string;
+  company: string;
+  registration: string | null;
+}
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -9,3 +9,4 @@ export * from './pipes/is-new.pipe';
 export * from './interceptor/http-error.interceptor';
 
 export * from './localization/date-adapter';
+export * from './localization/paginator';
