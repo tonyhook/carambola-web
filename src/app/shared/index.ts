df950fee3a36e80d01670360453f8a5805a0e917
Tony Hook <tonyhook.su@gmail.com>
2026-04-24 20:54:02 +0800

    [refactor] migrate async data flow to signals

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -3,8 +3,6 @@ export * from './components/operation/operation.component';
 export * from './components/permission/permission.component';
 export * from './components/treeview/treeview.component';
 
-export * from './pipes/get-role-name.pipe';
-export * from './pipes/get-user-name.pipe';
 export * from './pipes/is-new.pipe';
 
 export * from './interceptor/http-error.interceptor';
